28ab65284071255677cefe1fc5db62187081201e
Dita <dita@Ditas-MacBook-Pro.local>
2025-04-06 21:28:00 +0700

    finish exercise-1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
 import requests
+from io import BytesIO
+import os 
+import zipfile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -10,10 +13,23 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
-
+output_dir = '/Users/dita/Documents/Personals Projects/data-engineering-practice/Exercises/Exercise-1/output/'
 def main():
     # your code here
-    pass
+    os.makedirs(output_dir, exist_ok=True)
+
+    for i in download_uris:
+        filename = i.split("/")[-1]
+        filepath = os.path.join(output_dir,filename)
+        print(f"{filename} already downloaded")
+        response = requests.get(i)
+
+        if response.status_code==200:
+            with zipfile.ZipFile(BytesIO(response.content)) as zf:
+                zf.extractall(output_dir)
+                print("finish all")
+        else:
+            print("failed")
 
 
 if __name__ == "__main__":
